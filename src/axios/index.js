b14411a4aa635e3d03f20ae58502c8dc22ccc8c0
Sturrbon <sturrbon@outlook.com>
2019-01-25 00:37:56 +0800

    axios test
    测试京东登录接口

---
+++ src/axios/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+// axios.defaults.baseURL = 'http://119.23.228.131:8091/api/';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+// 添加请求拦截器
+// axios.interceptors.request.use(function (config) {
+//   // 在发送请求之前做些什么
+//   return config;
+// }, function (error) {
+//   // 对请求错误做些什么
+//   return Promise.reject(error);
+// });
+
+export default axios
--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Modal, Form, Input, Row, Col, message } from 'antd'
+import axios from '../axios'
 
 const FormItem = Form.Item
 const confirm = Modal.confirm
@@ -19,6 +20,11 @@ class Login extends Component {
     this.props.form.validateFields((err, value) => {
       if (!err) {
         console.log(value)
+        const params = value
+        params.token = 'cdddef32b7ec4be9926d30f545e76c37'
+        axios.post('/jingdong/login', { params }).then(res => {
+          console.log(res)
+        })
       }
     })
 
@@ -32,6 +38,19 @@ class Login extends Component {
 
   handleScan = () => {
     //
+    const params = {
+      name: '17098389419',
+      password: 'Tt7896357',
+      token: 'cdddef32b7ec4be9926d30f545e76c37'
+    }
+    // params.token = 'cdddef32b7ec4be9926d30f545e76c37'
+    axios.post('/api/jingdong/login', params).then(res => {
+      console.log(res)
+    })
+
+    // axios.get('/jingdong/getqrcode', {params:{ token: 'cdddef32b7ec4be9926d30f545e76c37'}}).then(res => {
+    //   console.log(res)
+    // })
   }
 
   handleClick = () => {
@@ -51,7 +70,7 @@ class Login extends Component {
             <a href="javascript:;" onClick={this.handleClick}>账户密码</a>
           </Col>
           <Col span={12}>
-            <a href="javscript:;" onClick={this.handleScan} >二维码</a>
+            <a href="javascript:;" onClick={this.handleScan} >二维码</a>
           </Col>
         </Row>
         <Modal
@@ -66,7 +85,7 @@ class Login extends Component {
           <Form>
             <FormItem
             >
-              {getFieldDecorator('userName', {
+              {getFieldDecorator('name', {
                 rules: [{
                   required: true, message: '请输入账户'
                 }]
@@ -76,7 +95,7 @@ class Login extends Component {
             </FormItem>
             <FormItem
             >
-              {getFieldDecorator('userPassword', {
+              {getFieldDecorator('password', {
                 rules: [{
                   required: true, message: '请输入密码'
                 }]
