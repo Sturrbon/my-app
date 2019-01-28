ec3f0cd63559db3feb098053b4cb17da99d239f3
Sturrbon <sturrbon@outlook.com>
2019-01-28 13:45:25 +0800

    json pretty

---
+++ src/mock.js
@@ -0,0 +1,240 @@
+export const data = {
+  "code": 200,
+  "data": {
+      "BtPrivilege": {
+      "interestFreeDays" : 30,
+      "txnFeeRate" : 0.6,
+      "interestRate" : 0.07,
+      "creditAccountId" : "180603027768421270203772",
+      "creditLimit" : 8800.0,
+      "availableLimit" : 8800.0,
+      "outstandingAmount" : 0.0,
+      "noIous" : 9,
+      "noIousUnpaid" : 0,
+      "noIousPaid" : 9,
+      "noIousInDlq" : 0,
+      "delinquencyInterest" : 0.0,
+      "delinquencyBalance" : 0.0,
+      "outstandingBalanceIn7Days" : 0.0,
+      "totalPaymentAmount" : 491.71,
+      "totalReversalAmount" : 0.0,
+      "totalFeeAmount" : 0.0,
+      "totalReversalFeeAmount" : 0.0,
+      "responseTs" : "32",
+      "version" : "3.0",
+      "refundCount" : 0,
+      "interestRulesList" : [ 
+          {
+              "planNum" : "1",
+              "feeType" : "1",
+              "baseValue" : "0",
+              "favValue" : "0",
+              "interestValue" : "0"
+          }, 
+          {
+              "planNum" : "3",
+              "feeType" : "1",
+              "baseValue" : "0.60000000",
+              "favValue" : "0",
+              "interestValue" : "0.60000000"
+          }, 
+          {
+              "planNum" : "6",
+              "feeType" : "1",
+              "baseValue" : "0.60000000",
+              "favValue" : "0",
+              "interestValue" : "0.60000000"
+          }, 
+          {
+              "planNum" : "12",
+              "feeType" : "1",
+              "baseValue" : "0.60000000",
+              "favValue" : "0",
+              "interestValue" : "0.60000000"
+          }, 
+          {
+              "planNum" : "24",
+              "feeType" : "1",
+              "baseValue" : "0.60000000",
+              "favValue" : "0",
+              "interestValue" : "0.60000000"
+          }
+      ],
+      "acountStatus" : 1,
+      "result" : {
+          "isSuccess" : true,
+          "code" : "00000",
+          "info" : "success"
+      }
+  },
+      "accountScore": "194",
+      "activityScore": "28",
+      "address": [
+          {
+              "address": "中德英伦联邦C区",
+              "area": "四川成都市双流区中和镇",
+              "email": "",
+              "name": "田某某",
+              "phone": "136****0114",
+              "telephone": ""
+          }, 
+      ],
+      "billLists": {
+      "billList" : [ 
+          {
+              "billId" : "201811033772",
+              "billAmt" : 95.8,
+              "payedAmt" : 95.8,
+              "refundAmt" : 0.0,
+              "refundSdpAmt" : 0.0,
+              "refundRepayAmt" : 0.0,
+              "planedNum" : 0,
+              "planedAmt" : 0.0,
+              "restPlanAmt" : 0.0,
+              "status" : 2,
+              "billDate" : "2018-11-03 00:00:00",
+              "billLimitDate" : "2018-11-12 23:59:59",
+              "sdpAmt" : 0.0,
+              "minPayAmt" : 0.0,
+              "nextSdpOverAmt" : 0.0,
+              "isCurBill" : 1,
+              "isInPayDay" : 0,
+              "realOverDays" : 0,
+              "isOverdueDay" : 1,
+              "overStatus" : 0,
+              "flag" : 0,
+              "sdpOverSum" : 0.0,
+              "result" : {
+                  "isSuccess" : false
+              }
+          },
+      ],
+      "currentPage" : 1,
+      "pageSize" : 10,
+      "pageCount" : 1,
+      "totalCount" : 5,
+      "result" : {
+          "isSuccess" : true
+      }
+  },
+      "btStatus": "3",
+      "btType": "1",
+      "card": [
+          {
+              "bank": "cmb",
+              "name": "*美玲",
+              "phone": "136****0114",
+              "saving": "储蓄卡",
+              "tailNum": "尾号1323"
+          },
+          {
+              "bank": "ccb",
+              "name": "*美玲",
+              "phone": "133****2344",
+              "saving": "储蓄卡",
+              "tailNum": "尾号1292"
+          }
+      ],
+      "consumptionScore": "1223",
+      "credit": "97.0 ",
+      "financeScore": "97",
+      "inserttime": "2018-11-27 18:00:24",
+      "loginName": "test网购i123",
+      "name": "test网购i123",
+      "needPayTotal": "300.89",
+      "nickName": "test网购i123",
+      "order": [
+          {
+              "address": "四川成都市高新区大源镇天府二街希尔顿大楼",
+              "amount": "269.00 ",
+              "dealtime": "2017-11-01 21:33:28",
+              "goodsName": "科颜氏（Kiehl's）亚马逊白泥净致面膜125ml（又名：亚马逊白泥净肤面膜）温和清洁去黑头",
+              "goodsNum": "x1",
+              "name": "李美玲",
+              "namrUrl": "https://item.jd.com/578436.html",
+              "orderid": "64286125618",
+              "payType": "在线支付",
+              "shop": "京东",
+              "status": "已完成",
+              "telephone": "133****2345"
+          },
+          {
+              "address": "四川成都市双流县中和镇中德英伦联邦C区18号楼1单元",
+              "amount": "64.70 ",
+              "dealtime": "2017-10-02 10:03:37",
+              "goodsName": "维达(Vinda) 手帕纸 细韧3层迷你纸巾*18包（新老包装替换中，有香及无香随机发货）",
+              "goodsNum": "x1",
+              "name": "李美玲",
+              "namrUrl": "https://item.jd.com/854224.html",
+              "orderid": "62195417009",
+              "payType": "在线支付",
+              "shop": "京东",
+              "status": "已完成",
+              "telephone": "133****2345"
+          },
+          {
+              "address": "四川成都市双流县中和镇中德英伦联邦C区18号楼1单元",
+              "amount": "90.00 ",
+              "dealtime": "2018-03-07 22:12:19",
+              "goodsName": "维达(Vinda) 抽纸 超韧3层150抽软抽纸巾*3包(小规格)",
+              "goodsNum": "x2",
+              "name": "赵三",
+              "namrUrl": "https://item.jd.com/854232.html",
+              "orderid": "71901758718",
+              "payType": "在线支付",
+              "shop": "京东",
+              "status": "已完成",
+              "telephone": "133****2345"
+          }
+      ],
+  "orderResult": {
+              "address_max_city_1m": "",
+              "address_max_city_3m": "",
+              "address_max_city_6m": "",
+              "address_max_detail_1m": "浙江杭州西湖区栖霞镇街道栖息大道南湖小区B区",
+              "address_max_detail_3m": "浙江杭州西湖区栖霞镇街道栖息大道南湖小区B区",
+              "address_max_detail_6m": "浙江杭州西湖区栖霞镇街道栖息大道南湖小区B区",
+              "address_max_district_1m": "",
+              "address_max_district_3m": "",
+              "address_max_district_6m": "",
+              "address_max_name_1m": "陈悠",
+              "address_max_name_3m": "陈悠",
+              "address_max_name_6m": "陈悠",
+              "address_max_phone_1m": "15628010612",
+              "address_max_phone_3m": "15628010612",
+              "address_max_phone_6m": "15628010612",
+              "address_max_province_1m": "浙江",
+              "address_max_province_3m": "浙江",
+              "address_max_province_6m": "浙江",
+              "order_avg_amount_1m": 23.66,
+              "order_avg_amount_3m": 67.852,
+              "order_avg_amount_6m": 60.055428571428585,
+              "order_avg_pkg_1m": 1.2,
+              "order_avg_pkg_3m": 1.25,
+              "order_avg_pkg_6m": 1.1428571428571428,
+              "order_max_amount_1m": 61,
+              "order_max_amount_3m": 259,
+              "order_max_amount_6m": 259,
+              "order_max_pkg_1m": 2,
+              "order_max_pkg_3m": 2,
+              "order_max_pkg_6m": 2,
+              "order_sum_1m": 5,
+              "order_sum_3m": 20,
+              "order_sum_6m": 35,
+              "order_sum_amount_1m": 118.3,
+              "order_sum_amount_3m": 1357.0400000000002,
+              "order_sum_amount_6m": 2101.9400000000005,
+              "order_sum_pkg_1m": 6,
+              "order_sum_pkg_3m": 25,
+              "order_sum_pkg_6m": 40
+          },        
+"permLimit": "8800.00",
+      "qqBind": "未绑定",
+      "riskScore": "4",
+      "totalScore": "2037",
+      "userType": "个人用户",
+      "wxBind": "绑定"
+  },
+  "m": "成功获取授权数据",
+  "s": 1
+}
--- src/view/list.js
+++ src/view/list.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import JSONPretty from 'react-json-pretty'
+import { data } from '../mock'
+import 'react-json-pretty/themes/monikai.css';
+var JSONPrettyMon = require('react-json-pretty/dist/monikai');
 
 class List extends Component {
   render() {
     return (
       <div>
         List
+        <JSONPretty theme={JSONPrettyMon} id="json-pretty" style={{fontSize: "1.1em", backgroundColor: '#eee'}} data={data}></JSONPretty>
       </div>
     )
   }
